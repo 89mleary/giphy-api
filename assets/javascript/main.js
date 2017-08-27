7361b605303cf76b5afd199bc54312f5920a2589
Mason Leary <Mason Leary>
2017-08-27 15:53:48 -0600

    Giphy App

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -28,7 +28,7 @@ $( document ).ready(function() {  // doc ready start
     $("#gif-area").empty();
     var animal = $(this).attr("data-animal");
     console.log(animal);
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
       animal + "&api_key=dc6zaTOxFJmzC&limit=10";
 
     $.ajax({
